c74ca44071d303d6a1dea31db5508137a697328c
Wilber Cutire <wil12fred@wil12fred.local>
2018-09-12 11:11:38 -0500

    basic configuration

--- leapmotion.h
+++ leapmotion.h
@@ -1,6 +1,5 @@
 #include "Leap.h"
 
-
 using namespace Leap;
 
 HandList hands;
@@ -57,62 +56,65 @@ void SampleListener::onFrame(const Controller& controller) {
             << ", tools: " << frame.tools().count()
             << ", gestures: " << frame.gestures().count() << std::endl;*/
 
+  if (frame.hands().isEmpty()){
+    NEWHAND=false;
+    return;
+  }
+  NEWHAND=true;
   hands = frame.hands();
-  bool existHand=false;
-  for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
-    existHand=true;
+
+  /*for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
     // Get the first hand
     const Hand hand = *hl;
     std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-    /*std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
-              << ", palm position: " << hand.palmPosition() << std::endl;*/
+    std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+              << ", palm position: " << hand.palmPosition() << std::endl;
     // Get the hand's normal vector and direction
     const Vector normal = hand.palmNormal();
     const Vector direction = hand.direction();
 
     // Calculate the hand's pitch, roll, and yaw angles
-    /*std::cout << std::string(2, ' ') <<  "pitch: " << direction.pitch() * RAD_TO_DEG << " degrees, "
+    std::cout << std::string(2, ' ') <<  "pitch: " << direction.pitch() * RAD_TO_DEG << " degrees, "
               << "roll: " << normal.roll() * RAD_TO_DEG << " degrees, "
-              << "yaw: " << direction.yaw() * RAD_TO_DEG << " degrees" << std::endl;*/
+              << "yaw: " << direction.yaw() * RAD_TO_DEG << " degrees" << std::endl;
 
     // Get the Arm bone
     Arm arm = hand.arm();
-    /*std::cout << std::string(2, ' ') <<  "Arm direction: " << arm.direction()
+    std::cout << std::string(2, ' ') <<  "Arm direction: " << arm.direction()
               << " wrist position: " << arm.wristPosition()
-              << " elbow position: " << arm.elbowPosition() << std::endl;*/
+              << " elbow position: " << arm.elbowPosition() << std::endl;
 
     // Get fingers
     const FingerList fingers = hand.fingers();
     for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
       const Finger finger = *fl;
-      /*std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
+      std::cout << std::string(4, ' ') <<  fingerNames[finger.type()]
                 << " finger, id: " << finger.id()
                 << ", length: " << finger.length()
-                << "mm, width: " << finger.width() << std::endl;*/
+                << "mm, width: " << finger.width() << std::endl;
 
       // Get finger bones
       for (int b = 0; b < 4; ++b) {
         Bone::Type boneType = static_cast<Bone::Type>(b);
         Bone bone = finger.bone(boneType);
-        /*std::cout << std::string(6, ' ') <<  boneNames[boneType]
+        std::cout << std::string(6, ' ') <<  boneNames[boneType]
                   << " bone, start: " << bone.prevJoint()
                   << ", end: " << bone.nextJoint()
-                  << ", direction: " << bone.direction() << std::endl;*/
+                  << ", direction: " << bone.direction() << std::endl;
       }
     }
-  }
+  }*/
 
   // Get tools
-  const ToolList tools = frame.tools();
+  /*const ToolList tools = frame.tools();
   for (ToolList::const_iterator tl = tools.begin(); tl != tools.end(); ++tl) {
     const Tool tool = *tl;
-    /*std::cout << std::string(2, ' ') <<  "Tool, id: " << tool.id()
+    std::cout << std::string(2, ' ') <<  "Tool, id: " << tool.id()
               << ", position: " << tool.tipPosition()
-              << ", direction: " << tool.direction() << std::endl;*/
-  }
-  NEWHAND=existHand;
+              << ", direction: " << tool.direction() << std::endl;
+  }*/
   // Get gestures
-  const GestureList gestures = frame.gestures();
+  /*const GestureList gestures = frame.gestures();
   for (int g = 0; g < gestures.count(); ++g) {
     Gesture gesture = gestures[g];
 
@@ -134,45 +136,45 @@ void SampleListener::onFrame(const Controller& controller) {
           CircleGesture previousUpdate = CircleGesture(controller.frame(1).gesture(circle.id()));
           sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * PI;
         }
-        /*std::cout << std::string(2, ' ')
+        std::cout << std::string(2, ' ')
                   << "Circle id: " << gesture.id()
                   << ", state: " << stateNames[gesture.state()]
                   << ", progress: " << circle.progress()
                   << ", radius: " << circle.radius()
                   << ", angle " << sweptAngle * RAD_TO_DEG
                   <<  ", " << clockwiseness << std::endl;
-        break;*/
+        break;
       }
       case Gesture::TYPE_SWIPE:
       {
         SwipeGesture swipe = gesture;
-        /*std::cout << std::string(2, ' ')
+        std::cout << std::string(2, ' ')
           << "Swipe id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", direction: " << swipe.direction()
-          << ", speed: " << swipe.speed() << std::endl;*/
+          << ", speed: " << swipe.speed() << std::endl;
         break;
       }
       case Gesture::TYPE_KEY_TAP:
       {
 	//game=!game;
         KeyTapGesture tap = gesture;
-        /*std::cout << std::string(2, ' ')
+        std::cout << std::string(2, ' ')
           << "Key Tap id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", position: " << tap.position()
-          << ", direction: " << tap.direction()<< std::endl;*/
+          << ", direction: " << tap.direction()<< std::endl;
         break;
       }
       case Gesture::TYPE_SCREEN_TAP:
       {
         ScreenTapGesture screentap = gesture;
-        /*std::cout << std::string(2, ' ')
+        std::cout << std::string(2, ' ')
           << "Screen Tap id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", position: " << screentap.position()
           << ", direction: " << screentap.direction()<< std::endl;
-        break;*/
+        break;
       }
       default:
         //std::cout << std::string(2, ' ')  << "Unknown gesture type." << std::endl;
@@ -181,8 +183,8 @@ void SampleListener::onFrame(const Controller& controller) {
   }
 
   if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
-    //std::cout << std::endl;
-  }
+    std::cout << std::endl;
+  }*/
 
 }
 
@@ -211,4 +213,3 @@ void SampleListener::onServiceConnect(const Controller& controller) {
 void SampleListener::onServiceDisconnect(const Controller& controller) {
   std::cout << "Service Disconnected" << std::endl;
 }
-
