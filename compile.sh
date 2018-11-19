e64a2abfa997815733718dabbd29c4db8131fc1a
Wilber Cutire <wil12fred@wil12fred.local>
2018-11-18 19:41:02 -0500

    comp

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #ESTA COMPILACIÓN ES PARA EL SISTEMA OPERATIVO OS X
-c++ -o main -std=c++11 client.cc -I./ -I./Leap -I./Model -Wno-deprecated -framework GLUT -framework OpenGL /usr/local/lib/libLeap.dylib -lpng
+c++ -o client -std=c++11 client.cc -I./ -I./Leap -I./Model -Wno-deprecated -framework GLUT -framework OpenGL /usr/local/lib/libLeap.dylib -lpng
 if [ $? -ne 0 ]; then 
 	echo "compilation error"
 else
-	install_name_tool -change @loader_path/libLeap.dylib ../lib/libLeap.dylib main
-	./main
+	install_name_tool -change @loader_path/libLeap.dylib ../lib/libLeap.dylib client
+	./client
 fi
