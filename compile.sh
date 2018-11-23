bb8eddb8a4440d563e2e57d72fe9f2e47827880b
Wilber Cutire <wil12fred@wil12fred.local>
2018-11-23 16:59:24 -0500

    xd

--- compile.sh
+++ compile.sh
@@ -6,3 +6,9 @@ else
 	install_name_tool -change @loader_path/libLeap.dylib ../lib/libLeap.dylib client
 	./client
 fi
+
+#UBUNTU
+#instalar 2.7+ leap sdk, carpeta en [LEAP]
+#instalar libpng
+#instalar glut
+#c++ -o client -std=c++11 client.cc -I./ -I./Leap -I./Model -Wno-deprecated -lGl -lGLU -lglut [LEAP]/LEAPSDK/lib/x64/libLeap.so -Wl,-rpath,[LEAP]/LeapSDK/lib/x64 -lpng
